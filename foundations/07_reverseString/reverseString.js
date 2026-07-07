e5bfc9c99db490a9d6b449bb76d9ddb4f82e44e2
jaredaitken6 <jaredaitken38@gmail.com>
2026-07-07 18:30:17 -0400

    finish 07_reverseString exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    let reversedStringArray = [];
 
+    for (const stringValue of stringArray) {
+    reversedStringArray.unshift(stringValue);
+    }
+
+    reversedStringArray = reversedStringArray.join('');
+
+    return reversedStringArray; 
 };
 
 // Do not edit below this line
