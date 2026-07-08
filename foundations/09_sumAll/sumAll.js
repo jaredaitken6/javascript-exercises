814905a2a1a09d06d1fffbea31f190fd27f40ef0
jaredaitken6 <jaredaitken38@gmail.com>
2026-07-08 02:29:43 -0400

    finish 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let lowestNum;
+    let highestNum;
+    let fullNumList = [];
+    let sum;
+// validate that numbers are positive
+// organize them to lowest/highest
+    if (Number.isInteger(num1) && Number.isInteger(num2) && num1 > 0 && num2 > 0) {
+        if (num1 > num2) {
+        highestNum = num1;
+        lowestNum = num2;
+        } else {
+        lowestNum = num1;
+        highestNum = num2;
+        }
+// create number list
+    for (let i = lowestNum; i <= highestNum; i++) {
+        fullNumList.push(i);
+    }
 
+// add number list
+    sum = fullNumList.reduce((acc, curr) => acc + curr, 0);
+
+    return sum;
+    
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
