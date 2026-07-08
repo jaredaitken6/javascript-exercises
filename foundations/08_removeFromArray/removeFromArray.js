6ebeb80f3a61a5191a34aa928db2980f8aa4cfe4
jaredaitken6 <jaredaitken38@gmail.com>
2026-07-08 00:57:14 -0400

    finish 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+
+    let removeArray = [];
+    let noDuplicateNumbers = [];
+
+    // turn arguments array into actual array
+    for (let i = 0; i < arguments.length; i++) {
+        if (i === 0) {
+            continue;
+        } else {
+            removeArray.push(arguments[i]);
+        }
+    }
+
+    // push value to noDuplicateNumbers array only if array value includes removeArray value
+    for (let i = 0; i < array.length; i++) {
+
+        if (!removeArray.includes(array[i])) {
+            noDuplicateNumbers.push(array[i]);
+        }
+
+    }
+
+    return noDuplicateNumbers;
+
 };
 
 // Do not edit below this line
