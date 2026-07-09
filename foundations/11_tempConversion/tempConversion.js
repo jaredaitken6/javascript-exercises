c8b429414cd9503b91f3594c798d1b48a3fb5b5b
jaredaitken6 <jaredaitken38@gmail.com>
2026-07-08 21:17:14 -0400

    finish 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  fah = (fah - 32) * 5 / 9;
+  return Number(fah.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+
+  cel = (cel * 1.8) + 32;
+  return Number(cel.toFixed(1));
+
 };
 
 // Do not edit below this line
