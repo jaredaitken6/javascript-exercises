6ac2acebf9b3b13a1a4b229a625264f515f4cae9
jaredaitken6 <jaredaitken38@gmail.com>
2026-07-22 23:42:18 -0400

    complete 15_getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+
+  const bookTitles = books.reduce((acc, currentBook) => {
+    acc.push(currentBook.title);
+    return acc;
+  }, []);
+
+  return bookTitles;
 
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
