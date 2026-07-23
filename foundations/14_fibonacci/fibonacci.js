396570c275676162a91154d5b14135476ab9c495
jaredaitken6 <jaredaitken38@gmail.com>
2026-07-22 22:57:38 -0400

    complete 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(indexNum) {
+    let fiboArray = [0, 1];
+    indexNum = Number(indexNum);
 
+    if (indexNum < 0) {
+        return 'OOPS';
+    } else {
+
+    for (let i = 0; i < indexNum - 1; i++) {
+        fiboArray.push((fiboArray[fiboArray.length - 1]) + (fiboArray[fiboArray.length - 2]));
+    }
+
+    return fiboArray[indexNum];
+    
+    }
 };
 
 // Do not edit below this line
