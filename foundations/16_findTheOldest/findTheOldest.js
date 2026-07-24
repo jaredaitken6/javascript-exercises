c0a7d16a27294a27ec41dc80cd59e1eaf33ffdf4
jaredaitken6 <jaredaitken38@gmail.com>
2026-07-23 22:40:34 -0400

    complete 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let thisYear = new Date();
+    
+  const yearsAlive = people.map((value) => {
+    if (value.yearOfDeath === undefined) {
+      value.yearOfDeath = thisYear.getFullYear()
+    }
+    return value.yearOfDeath - value.yearOfBirth;
+  });
+
+  for (let i = 0; i < people.length; i++) {
+    people[i].yearsLiving = yearsAlive[i];
+  }
+
+
+people.sort((a, b) => b.yearsLiving - a.yearsLiving);
+
+return people[0];
 
 };
 
